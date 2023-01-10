2ce31c160057ad520ea0e50d23c91a426ffbfcbc
Elkaouni <elkaouni.yousra@gmail.com>
2023-01-10 21:53:14 +0000

    hi

--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,137 @@ import { EcoreTreeDataProvider, EcoreModel, EcoreNode } from "./treeview";
 export function activate(context: vscode.ExtensionContext) {
 	const model = new EcoreModel([
 		new EcoreNode('EPackage', 'Package 1', [
-			new EcoreNode('EClass', 'My Class')
+			new EcoreNode('EClass', 'Class 1', [
+				new EcoreNode('EAttribute', 'Attribute 1')
+			])
 		]),
-		new EcoreNode('EPackage', 'Package 2')
+		new EcoreNode('EPackage', 'Package 2', [
+			new EcoreNode('EClass', 'Class 2')
+		]),
 	]);
 
-	vscode.window.registerTreeDataProvider('exampleView', new EcoreTreeDataProvider(model));
+	const ecoreTreeDataProvider = new EcoreTreeDataProvider(model);
+	vscode.window.registerTreeDataProvider('exampleView', ecoreTreeDataProvider);
 
+
+	const treeView = vscode.window.createTreeView('exampleView', { treeDataProvider: ecoreTreeDataProvider });
+	context.subscriptions.push(treeView);
+
+	//show context menu
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.openContextMenu', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`ContextMenu for node ${node.getName()}`);
+	}));
+
+	//execute command when sleceted from context menu
+	context.subscriptions.push(treeView.onDidChangeSelection(async (event) => {
+        if (event.selection.length > 0) {
+			const selectedNode: EcoreNode = event.selection[0];
+			if(event.selection){
+			const actions = [
+				{ label: 'Show Properties', command: 'VMSC.openContextMenu' },
+				{ label: 'Rename', command: 'VMSC.rename' },
+				{ label: 'Delete', command: 'VMSC.delete' },
+				{ label: 'Add Package', command: 'VMSC.addPackage' },
+				{ label: 'Add Class ', command: 'VMSC.addClass' },
+				{ label: 'Add Association', command: 'VMSC.addAssociation' },
+				{ label: 'Add Generalization', command: 'VMSC.addGeneralization' },
+				{ label: 'Add Enumeration', command: 'VMSC.addEnumeration' },
+				{ label: 'Add Literal', command: 'VMSC.addLiteral' },
+				{ label: 'Add Date Type', command: 'VMSC.addDateType' },
+				{ label: 'Add Attribute', command: 'VMSC.addAttribute' },
+				{ label: 'Add Operation', command: 'VMSC.addOperation' },
+				{ label: 'Add Annotation', command: 'VMSC.addAnnotation' }
+				
+			];
+			const result = await vscode.window.showQuickPick(actions);
+			if (result) {
+				vscode.commands.executeCommand(result.command, selectedNode);
+			}
+		}
+		}
+	}));
+
+	//rename node
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.rename', async (node: EcoreNode) => {
+		const result = await vscode.window.showInputBox({ prompt: `Rename ${node.getName()}` });
+		if(result)
+		{
+			node.setName(result);
+			//model.rootNodes.forEach(element => { element.getId() == node.getId()? element.setName(result): null;});
+			ecoreTreeDataProvider?.getonDidChangeTreeData().fire();
+			vscode.window.showInformationMessage(`Renamed node to ${node.getName()}`)
+		}
+	}));	
+
+	//delete node
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.delete', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Delete ${node.getName()}`);
+		const parent = node.parent;
+		const index = parent.children.indexOf(node);
+		parent.children.splice(index, 1);
+		ecoreTreeDataProvider.getonDidChangeTreeData().fire();
+		//node.delete();	
+	}));
+
+	//add package
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.addPackage', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Add Package`);
+		//node.addPackage();	
+	}));
+
+	//add class
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.addClass', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Add Class`);
+		//node.addClass();	
+	}	));
+
+	//add association
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.addAssociation', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Add Association`);
+		//node.addAssociation();	
+	}));
+
+	//add generalization
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.addGeneralization', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Add Generalization`);
+		//node.addGeneralization();
+	}));
+
+	//add enumeration
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.addEnumeration', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Add Enumeration`);
+		//node.addEnumeration();
+	}));
+
+	//add literal
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.addLiteral', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Add Enumeration`);
+		//node.addLiteral();
+	}));
+
+	//add date type
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.addDateType', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Add Date Type`);
+		//node.addDateType();
+	}));
+
+	//add attribute
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.addAttribute', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Add Attribute`);
+		//node.addAttribute();
+	}));
+
+	//add operation
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.addOperation', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Add Operation`);
+		//node.addOperation();
+	}));
+
+	//add annotation
+	context.subscriptions.push(vscode.commands.registerCommand('VMSC.addAnnotation', (node: EcoreNode) => {
+		vscode.window.showInformationMessage(`Add Operation`);
+		//node.addAnnotation();
+	}));
 }
 
-export function deactivate() { }
+
--- src/treeview.ts
+++ src/treeview.ts
@@ -1,42 +1,36 @@
 import * as vscode from "vscode";
 
-// export class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
-//   onDidChangeTreeData?: vscode.Event<TreeItem | null | undefined> | undefined;
-
-//   data: TreeItem[];
-
-//   constructor() {
-//     this.data = [new TreeItem('cars', [
-//       new TreeItem(
-//         'Ford', [new TreeItem('Fiesta'), new TreeItem('Focus'), new TreeItem('Mustang')]),
-//       new TreeItem(
-//         'BMW', [new TreeItem('320'), new TreeItem('X3'), new TreeItem('X5')])
-//     ])];
-//   }
-
-//   getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
-//     return element;
-//   }
-
-//   getChildren(element?: TreeItem | undefined): vscode.ProviderResult<TreeItem[]> {
-//     if (element === undefined) {
-//       return this.data;
-//     }
-//     return element.children;
-//   }
-// }
-
-// class TreeItem extends vscode.TreeItem {
-//   children: TreeItem[] | undefined;
-
-//   constructor(label: string, children?: TreeItem[]) {
-//     super(
-//       label,
-//       children === undefined ? vscode.TreeItemCollapsibleState.None :
-//         vscode.TreeItemCollapsibleState.Expanded);
-//     this.children = children;
-//   }
-// }
+/* export class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
+   onDidChangeTreeData?: vscode.Event<TreeItem | null | undefined> | undefined;/*
+   data: TreeItem[];/*
+   constructor() {
+     this.data = [new TreeItem('cars', [
+       new TreeItem(
+         'Ford', [new TreeItem('Fiesta'), new TreeItem('Focus'), new TreeItem('Mustang')]),
+       new TreeItem(
+         'BMW', [new TreeItem('320'), new TreeItem('X3'), new TreeItem('X5')])
+     ])];
+   }/*
+   getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+     return element;
+   }/*
+   getChildren(element?: TreeItem | undefined): vscode.ProviderResult<TreeItem[]> {
+     if (element === undefined) {
+       return this.data;
+     }
+     return element.children;
+   }
+ }/*
+ class TreeItem extends vscode.TreeItem {
+   children: TreeItem[] | undefined;/*
+   constructor(label: string, children?: TreeItem[]) {
+     super(
+       label,
+       children === undefined ? vscode.TreeItemCollapsibleState.None :
+         vscode.TreeItemCollapsibleState.Expanded);
+     this.children = children;
+   }
+ }*/
 
 export class EcoreTreeDataProvider implements vscode.TreeDataProvider<EcoreNode> {
   private _onDidChangeTreeData: vscode.EventEmitter<EcoreNode | undefined> = new vscode.EventEmitter<EcoreNode | undefined>();
@@ -44,51 +38,54 @@ export class EcoreTreeDataProvider implements vscode.TreeDataProvider<EcoreNode>
 
   constructor(private readonly ecoreModel: EcoreModel) { }
 
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getonDidChangeTreeData(){
+    return this._onDidChangeTreeData;
+  }
+
   getTreeItem(element: EcoreNode): vscode.TreeItem {
+
+    let treeItem: vscode.TreeItem = {
+      label: element.getName(),
+      collapsibleState: vscode.TreeItemCollapsibleState.None,
+      contextValue: element.type,
+      command: {
+        command: 'VMSC.openContextMenu',
+        title: 'openContextMenu',
+        arguments: [element]
+      }
+    };
+
     // Return a TreeItem based on the type of element
     switch (element.type) {
       case 'EClass':
-        return {
-          label: element.name,
-          collapsibleState: vscode.TreeItemCollapsibleState.Collapsed,
-          contextValue: 'EClass'
-        };
-      case 'EAttribute':
-        return {
-          label: element.name,
-          collapsibleState: vscode.TreeItemCollapsibleState.None,
-          contextValue: 'EAttribute'
-        };
-      case 'EReference':
-        return {
-          label: element.name,
-          collapsibleState: vscode.TreeItemCollapsibleState.None,
-          contextValue: 'EReference'
-        };
-      case 'EDataType':
-        return {
-          label: element.name,
-          collapsibleState: vscode.TreeItemCollapsibleState.None,
-          contextValue: 'EDataType'
-        };
+        treeItem.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
+        break;
       case 'EPackage':
-        return {
-          label: element.name,
-          collapsibleState: vscode.TreeItemCollapsibleState.Collapsed,
-          contextValue: 'EPackage'
-        };
+        treeItem.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
+        break;
+      case 'EAttribute':
+        break;
+      case 'EReference':
+        break;
+      case 'EDataType':
+        break;
+      case 'EOperation':
+        break;
+      case 'EAnnotation':
+        break;
       case 'EConstraint':
-        return {
-          label: element.name,
-          collapsibleState: vscode.TreeItemCollapsibleState.None,
-          contextValue: 'EConstraint'
-        };
+        break;
       default:
         return {
           label: 'Unknown Element',
           collapsibleState: vscode.TreeItemCollapsibleState.None
         };
     }
+    return treeItem;
   }
 
   getChildren(element?: EcoreNode): vscode.ProviderResult<EcoreNode[]> {
@@ -98,17 +95,61 @@ export class EcoreTreeDataProvider implements vscode.TreeDataProvider<EcoreNode>
     }
 
     // Otherwise, return the children of the element
-    return element.children;
+    return element.getChildren();
   }
 }
 
 export class EcoreNode {
+    private readonly id: string= (genUniqueId() as string);
+
   constructor(
-    public readonly type: 'EClass' | 'EAttribute' | 'EReference' | 'EDataType' | 'EPackage' | 'EConstraint',
-    public readonly name: string,
-    public readonly children: EcoreNode[] = []
-  ) { }
-}
+    public readonly type: 'EClass' | 'EAttribute' | 'EReference' | 'EDataType' |
+    'EOperation' | 'EAnnotation' | 'EPackage' | 'EConstraint',
+    private name: string,
+    private children: EcoreNode[] = []
+  ) {}
+
+  getId() {
+    return this.id;
+  }
+
+  setName(name: string) {
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+
+  getChild(index: number) {
+    return this.children[index];
+  }
+  setChild(index: number, child: EcoreNode) {
+    this.children[index] = child;
+  }
+
+  getChildren() {
+    return this.children;
+  }
+  setChildren(children: EcoreNode[]) {
+    this.children = children;
+  }
+
+} 
+
 export class EcoreModel {
   constructor(public readonly rootNodes: EcoreNode[]) { }
 }
+
+function genUniqueId(): string {
+  const dateStr = Date
+    .now()
+    .toString(36); // convert num to base 36 and stringify
+
+  const randomStr = Math
+    .random()
+    .toString(36)
+    .substring(2, 8); // start at index 2 to skip decimal point
+
+  return `${dateStr}-${randomStr}`;
+}
+export default genUniqueId;
